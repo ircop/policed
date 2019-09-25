565d4d9b0b5b0a53814a582fa9b0fe69819123ce
ircop <wingman@ip-home.net>
2019-09-26 00:03:41 +0300

    Variable burst & allow burst changing

--- policied.go
+++ policied.go
@@ -1,6 +1,7 @@
 package policied
 
 import (
+	"errors"
 	"math"
 	"net"
 	"sync"
@@ -11,21 +12,30 @@ import (
 	"golang.org/x/time/rate"
 )
 
+var ErrWrongBurstFactor = errors.New("burst factor out of range. Should be 1 < factor < 0.001")
+
 type Policier struct {
-	globalBps uint64
-	connBPS   uint64
-	connPool  sync.Map
+	globalBps   uint64
+	connBPS     uint64
+	connPool    sync.Map
+	burstFactor BurstFactor
 
 	limiter  *rate.Limiter
 	maxChunk uint64
+}
+
+type BurstFactor struct {
+	factor float64
+	mu     sync.Mutex
 }
 
 // Creates new policier with given bandwidth limit in KBps (kiloBYtes)
 // If bandwidth is zero, there is no limit
 func NewPolicier(gloablKBPS uint64, connKBPS uint64) *Policier {
 	policier := &Policier{
-		connBPS: connKBPS * 1024,
-		limiter: rate.NewLimiter(rate.Limit(0), 0),
+		burstFactor: BurstFactor{factor: 0.005},
+		connBPS:     connKBPS * 1024,
+		limiter:     rate.NewLimiter(rate.Limit(0), 0),
 	}
 	policier.SetGlobalRate(gloablKBPS)
 
@@ -59,21 +69,43 @@ func (p *Policier) SetGlobalRate(kbps uint64) {
 		atomic.StoreUint64(&p.globalBps, globalRate)
 	}
 
-	// Calculate maximum chunk size.
-	// We don't want single conn to occupy whole global limit
-	var maxChunk uint64 = 128 * 1024
-	if globalRate > 0 {
-		maxChunk = uint64(math.Ceil(float64(globalRate) * 0.005))
+	p.setLimiterParams(globalRate, p.burstFactor.Get())
+}
+
+// SetBurstFactor sets... a burst factor :) which is, actually, the amount of time to allow a burst of traffic
+// at the full line rate of a policed connection.
+// I.e if rate limit is 10000 (bytes per second), and burst factor is 0.005 (s, or 5ms), actual burst will be
+// 10000 * 0.005 => 50 bytes
+// Usually you do not need to adjust it, but you may want it with very high or very low limit rates.
+// Value should be between 0 and 1000. Default is 5.
+// The lower the burst factor, the more accurate is rate limiting, but the more resources it consumes. And vice versa.
+func (p *Policier) SetBurstFactor(factor float64) error {
+	if err := p.burstFactor.Set(factor); err != nil {
+		return err
 	}
+	globalRate := atomic.LoadUint64(&p.globalBps)
+
+	p.setLimiterParams(globalRate, factor)
+
+	return nil
+}
+
+// (re)calculate max chunk
+// replace limiter
+// set conn's parameters
+func (p *Policier) setLimiterParams(globalRate uint64, burstFactor float64) {
+	// calculate max chunk. If global rate is 0, max. chunk is also 0
+	var maxChunk uint64
+	limit := rate.Inf
+	if globalRate > 0 {
+		maxChunk = uint64(math.Ceil(float64(globalRate) * burstFactor))
+		limit = rate.Limit(globalRate)
+	}
+
 	atomic.StoreUint64(&p.maxChunk, maxChunk)
+	p.setLimiter(rate.NewLimiter(limit, int(maxChunk)))
 
 	p.setConnMaxChunk(maxChunk)
-
-	limit := rate.Limit(globalRate)
-	if globalRate == 0 {
-		limit = rate.Inf
-	}
-	p.setLimiter(rate.NewLimiter(limit, int(maxChunk)))
 }
 
 // Wrap Accept() so that we can just replace
@@ -86,7 +118,9 @@ func (p *Policier) Wrap(conn net.Conn, err error) (*WrappedConn, error) {
 	// wrap and do stuff
 	sizes := make(chan uint64)
 	permits := make(chan struct{}, 1)
-	wrapped := WrapConn(conn, p.connBPS, atomic.LoadUint64(&p.maxChunk), sizes, permits)
+	wrapped := WrapConn(conn, p.connBPS, atomic.LoadUint64(&p.maxChunk), &p.burstFactor,
+		sizes, permits)
+
 	go p.listenConn(sizes, permits)
 
 	// this will not work for unix sockets or pipes, but i assume we will not serve
@@ -154,4 +188,20 @@ func (p *Policier) getCurrentLimiter() *rate.Limiter {
 // replace current limiter with new one
 func (p *Policier) setLimiter(limiter *rate.Limiter) {
 	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&p.limiter)), unsafe.Pointer(limiter))
+}
+
+func (b *BurstFactor) Set(factor float64) error {
+	if factor < 0.001 || factor > 1 {
+		return ErrWrongBurstFactor
+	}
+	b.mu.Lock()
+	b.factor = factor
+	b.mu.Unlock()
+
+	return nil
+}
+func (b *BurstFactor) Get() float64 {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	return b.factor
 }
